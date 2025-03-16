6aa11e971976ae16e338dce5ef6fe7ceee5d75d9
mattp532 <pmatt.212@gmail.com>
2025-03-16 03:15:18 -0400

    Change db

--- Backend/db.js
+++ Backend/db.js
@@ -3,11 +3,11 @@ const { Pool } = require('pg');
 require('dotenv').config();  
 
 const pool = new Pool({
-  user:"postgres",
-  password:"Wordpass321*",
-  host: "localhost",
-  port: 5432,
-  database:"Composerdle"
-});
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
+  });
 
 module.exports=pool
